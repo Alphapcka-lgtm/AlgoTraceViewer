396253e6aa13f007f7bd40742c4f935e361af952
Johannes Reck <johannes.c.reck@gmx.de>
2026-04-08 21:43:55 +0200

    first set container animation

--- frontend/src/SVGOutput.tsx
+++ frontend/src/SVGOutput.tsx
@@ -1,4 +1,6 @@
 import gsap from "gsap";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+import MorphSVGPlugin from "gsap/MorphSVGPlugin";
 import {Edges} from "./Edges.tsx";
 import {StaticNodes} from "./Nodes.tsx";
 
@@ -10,17 +12,16 @@ export function SVGOutput(props: SVGOutputProps) {
     const [isPlaying, setIsPlaying] = useState(false);
     const tlRef = useRef<gsap.core.Timeline>(null);
 
-    useGSAP(() => {
-        tlRef.current = gsap.timeline({ paused: true });
+    gsap.registerPlugin(DrawSVGPlugin);
+    gsap.registerPlugin(MorphSVGPlugin);
 
-        props.output.initialState.nodes.forEach(node => {
-            const el = document.getElementById(node.id);
-            if(el) {
-                const x = 200 * (Math.random() - 0.5);
-                const y = 200 * (Math.random() - 0.5);
-                tlRef.current?.to(el, {x, y, duration: 4, repeat: -1, yoyo: true}, "test")
-            }
-        });
+    useGSAP(() => {
+        tlRef.current = gsap.timeline({ paused: true } );
+
+
+        tlRef.current?.from("#i1", {duration: 2, drawSVG: "50% 50%"}, "step1")
+
+        tlRef.current?.to("#i1", {duration: 2, morphSVG: "#i2"})
 
     }, [props.output.initialState]);
 
@@ -28,6 +29,8 @@ export function SVGOutput(props: SVGOutputProps) {
         <svg height={props.height} style={{ border: "1px solid black", borderRadius: "30px" }}>
             <Edges edges={props.output.initialState.edges} nodes={props.output.initialState.nodes} />
             <StaticNodes nodes={props.output.initialState.nodes} />
+            <path id="i1" d={getContainerPath(100, 400,800, 0, 20)} style={{stroke: "black", fill: "none"}}/>
+            <path id="i2" d={getContainerPath(100, 400,800, 20, 20)} style={{display: "none"}}/>
         </svg>
         <div>
             <button style={{width: "50%"}} onClick={() => {
@@ -51,4 +54,18 @@ export function SVGOutput(props: SVGOutputProps) {
             props.onChangeInput()
         }}>Change Input</button>
     </> : <></>;
+}
+
+function getContainerPath(x: number, y: number, w: number, h: number, r: number) : string {
+    let z = "";
+    z = z + "M " + (x+r) + " " + (y) + " ";
+    z = z + "L " + (x+r+w) + " " + (y) + " ";
+    z = z + "A " + (r) + " " + (r) + " 0 0 1 " + (x+r+w+r) + " " + (y+r) + " ";
+    z = z + "L " + (x+r+w+r) + " " + (y+r+h) + " ";
+    z = z + "A " + (r) + " " + (r) + " 0 0 1 " + (x+r+w) + " " + (y+r+h+r) + " ";
+    z = z + "L " + (x+r) + " " + (y+r+h+r) + " ";
+    z = z + "A " + (r) + " " + (r) + " 0 0 1 " + (x) + " " + (y+r+h) + " ";
+    z = z + "L " + (x) + " " + (y+r) + " ";
+    z = z + "A " + (r) + " " + (r) + " 0 0 1 " + (x+r) + " " + (y) + " ";
+    return z;
 }
