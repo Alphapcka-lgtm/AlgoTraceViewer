2eb7f50b437bb4a5d70955cf7feff86cadc6878c
Johannes Reck <johannes.c.reck@gmx.de>
2026-06-09 16:10:54 +0200

    removed ehrlich tests

--- frontend/src/ehrlichSwaps/input/Types.tsx
+++
@@ -1,8 +0,0 @@
-import type { AnimationRequest } from "../shared/Types.tsx";
-import type { Dispatch, SetStateAction } from "react";
-
-export type SVGInputProps = {
-    input: AnimationRequest,
-    setInput: Dispatch<SetStateAction<AnimationRequest>>,
-    onSubmit: () => void;
-};
