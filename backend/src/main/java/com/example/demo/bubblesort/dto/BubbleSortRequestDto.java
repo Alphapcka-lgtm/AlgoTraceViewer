378dc7a6285026e7539b6a63b74d67619fb3a6a8
constantin <murr.c@outlook.com>
2026-08-15 15:48:54 +0200

    altes zeug gelöscht.

--- backend/src/main/java/com/example/demo/bubblesort/dto/BubbleSortRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.bubblesort.dto;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record BubbleSortRequestDto(List<Integer> numbers) {
-}
--- backend/src/main/java/com/example/demo/bubblesort/entities/BubbleSortState.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.bubblesort.entities;
-
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record BubbleSortState(List<Integer> numbers) {
-}
